5645dcd0257d611825752668e6d371c73f6e16ee
antunmod <antun.mod@gmail.com>
2017-09-04 22:51:04 +0200

    Created Scrolling Activity

--- app/src/main/java/antunmod/projects/travelpacking/MainActivity.java
+++ app/src/main/java/antunmod/projects/travelpacking/MainActivity.java
@@ -1,13 +1,26 @@
 package antunmod.projects.travelpacking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView packTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        packTextView = (TextView) findViewById(R.id.packTextView);
+        packTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchIntent = new Intent (view.getContext(), ScrollingActivity.class);
+                startActivity(switchIntent);
+            }
+        });
     }
 }
