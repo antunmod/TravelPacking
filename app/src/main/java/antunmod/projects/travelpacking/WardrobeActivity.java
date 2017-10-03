6122c4c4b45869126973e48b0aee48b1cc57ea95
antunmod <antun.mod@gmail.com>
2017-10-03 15:42:03 +0200

    Image gallery working
    The gallery is slow and is reading dummy images which were shrunken to enhance speed

--- app/src/main/java/antunmod/projects/travelpacking/WardrobeActivity.java
+++ app/src/main/java/antunmod/projects/travelpacking/WardrobeActivity.java
@@ -3,6 +3,7 @@ package antunmod.projects.travelpacking;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
@@ -26,11 +27,29 @@ public class WardrobeActivity extends AppCompatActivity {
     private ArrayList<ImageItem> getData() {
         final ArrayList<ImageItem> imageItems = new ArrayList<>();
         TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
-        for (int i = 0; i < imgs.length(); i++) {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        for (int i = 0; i < 3*imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i%imgs.length(), -1));
+            //bitmap = getResizedBitmap(bitmap, 200, 200);
+            imageItems.add(new ImageItem(bitmap, "Testing text size: -----------------------------Image#" + i));
         }
         return imageItems;
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
     }
 }
 
